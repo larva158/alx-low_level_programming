efd67ad0de5d02bd2c1f213f4ebd2817274de582
larva158 <yaoboris01@icloud.com>
2022-06-02 18:12:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - begining
+ *
+ * return 0 - ending
+ */
 int main(void)
 { 
-	puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
  	return (0);
 }
