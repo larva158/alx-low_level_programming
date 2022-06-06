9d9f8731a3c977ea475d0573726798af06c26eef
larva158 <yaoboris01@icloud.com>
2022-06-05 18:52:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+		if (alphabet == ('q' || 'e'))
+		{
+			putchar('\0');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
