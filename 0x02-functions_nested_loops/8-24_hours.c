e1032b229434d69121f1d5db5b390985d498db3d
larva158 <yaoboris01@icloud.com>
2022-06-08 09:49:45 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+
+{
+	int j;
+	int k;
+
+	for (j = 0; j <= 23; j++)
+	{
+		for (k = 0; k <= 59; k++)
+		{
+			_putchar (j / 10 + '0');
+			_putchar (j % 10 + '0');
+			_putchar (':');
+			_putchar (k / 10 + '0');
+			_putchar (k % 10 + '0');
+			_putchar ('\n');
+
+		}
+
+
+	}
+
+}
