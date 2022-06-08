b3dd9080134725e5a853fb378e92343d2d25afe9
larva158 <yaoboris01@icloud.com>
2022-06-07 18:40:20 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <main.h>
+/**
+ * _putchar - writes the character to stdout
+ *
+ * Return: On success
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 8);
+	return (0);
+}
