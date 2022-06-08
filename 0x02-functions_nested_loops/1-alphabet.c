ab959c189aa875901cfdb1d51100cf6d258f096d
larva158 <yaoboris01@icloud.com>
2022-06-08 07:57:51 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet - print alphabet in lowercase
+ *
+ *return: always 0
+*/
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
