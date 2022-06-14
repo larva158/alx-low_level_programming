ce82b461e1b590d368564946232c6298c30e4711
larva158 <yaoboris01@icloud.com>
2022-06-14 16:34:53 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take pointer n and update his value
+ *@n:pointer
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
