a982fdec02a3743d5186603d943775a5277ff17d
larva158 <yaoboris01@icloud.com>
2022-06-14 18:30:54 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an array of integers
@@ -19,5 +20,5 @@ void print_array(int *a, int n)
 			printf(", ");
 		}
 	}
-	_putchar("\n");
+	printf("\n");
 }
