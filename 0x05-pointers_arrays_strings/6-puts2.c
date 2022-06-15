32dc2e6df5cd8ad5ecccf3fc0f1c31c89badb53a
larva158 <yaoboris01@icloud.com>
2022-06-14 18:02:10 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  puts2 - prints every other character of a string
+ *@str: pointer
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar (str[j]);
+		}
+	}
+	_putchar ('\n');
+}
