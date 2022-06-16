94c9257db91318ca158a03372d15b0b2abf3cfd2
larva158 <yaoboris01@icloud.com>
2022-06-15 18:09:47 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ *@dest: pointer
+ *@src: pointer
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char s1[50], s2[50];
+
+	strcpy(s1,  "Hello ");
+	strcpy(s2, "World!\n");
+	_strcat(dest, src);
+	_putchar(*dest);
+
+	return (0);
+}
