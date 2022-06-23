8fa676d1f6cdb9839ee1175028e7994e6ba22b61
larva158 <yaoboris01@icloud.com>
2022-06-23 09:21:20 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - function that returns the pow
+ * @x: number
+ * @y: number
+ * Return : end of the function
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	return (1);
+}
