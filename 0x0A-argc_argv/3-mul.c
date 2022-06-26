06bc28906f597a374c8647ba627d1afb983cb1bf
larva158 <yaoboris01@icloud.com>
2022-06-26 05:16:33 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - begining of the algorithm
+ * @argc: number of argument
+ * @argv: array
+ * Return: succes(0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int nb1 , nb2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	nb1 = atoi(argv[1]);
+	nb2 = atoi(argv[2]);
+	result = nb1 * nb2;
+	printf("%d\n", result);
+	return (0);
+}
