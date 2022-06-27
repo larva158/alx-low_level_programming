a465be6bfe93de3bda79649e8d3e112b260cf0ee
larva158 <yaoboris01@icloud.com>
2022-06-26 17:46:39 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - begining of the algorithm
+ * @argc: number of argument
+ * @argv: array
+ * Return: succes(0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	/*if (argv[i] == 'e')
+	{
+		printf("Error");
+		return (1);
+	}*/
+	if (argc > 1 && (sum % sum == 0))
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
