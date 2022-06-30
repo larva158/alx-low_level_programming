a80f2e4572a9bc1947c244fab2a1d930d54172f5
larva158 <yaoboris01@icloud.com>
2022-06-29 19:34:41 -0700

    libmy.a, main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet(void);
@@ -12,3 +15,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
