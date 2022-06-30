4676166b64a268ddc955200abbc601ace647544f
larva158 <yaoboris01@icloud.com>
2022-06-29 17:59:54 -0700

    libmy.a, main.h

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * int main(void): entry point
+ * Return: 0 succes
+ */
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
