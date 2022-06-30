d1d89c1281ddce11cb5ea0b08da28eeb9fb9b18f
larva158 <yaoboris01@icloud.com>
2022-06-29 17:56:03 -0700

    libmy.a, main.h

--- 0x09-static_libraries/strstr.c
+++ 0x09-static_libraries/strstr.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+ * _strstr - funtion _strstr
+ * @haystack: char
+ * @needle: char
+ * Return: 0 succes
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
 	(void) haystack;
 	(void) needle;
 	printf("strstr\n");
+	return (0);
 }
