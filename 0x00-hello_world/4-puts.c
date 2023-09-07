ecd50df28854249a3707224c25bbbf5ddc289064
larva158 <yaoboris01@icloud.com>
2023-09-07 12:28:43 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program that print a line with puts function
+ *
+ * Return: 0 succes
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
